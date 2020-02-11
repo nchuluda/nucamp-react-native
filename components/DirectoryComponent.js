fed3a58262475ebdcf8a878c7e6c51543f168581
Nathan Chuluda <nchuluda@gmail.com>
2020-02-11 10:42:20 -0500

    Activity Indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -30,6 +32,17 @@ class Directory extends Component {
                 />
             );
         };
+
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{props.campsites.errMess}</Text>
+                </View>
+            );
+        }
 
         return (
             <FlatList 
